05be7ebe9c2e50d628bff6a65c02b9a8c53a141c
Daniel <daniel@Daniels-MacBook-Pro.local>
2021-03-01 13:25:48 -0700

    Can generate random numbers now

--- src/features/random-number/RandomNumber.tsx
+++ src/features/random-number/RandomNumber.tsx
@@ -42,13 +42,22 @@ const RandomNumber = () => {
         if (n < minNumber) setMinNumber(n);
     }
 
+    // generate random number
+    const genRandom = () => {
+        const nItems = maxNumber - minNumber + 1;
+        const newNumber = Math.floor(Math.random() * (nItems)) + minNumber;
+
+        setRandomNumber(newNumber);
+        setGo(true);
+    }
+
     return (
         <div>
             <Spinner speed={speed} go={go}>
                 {positions.map((p, idx) => <Circle key={idx} pos={p} size={circleSize} text={go ? '' : (minNumber + idx).toString()} spin={!go}/>)}
             </Spinner>
 
-            <Button className="rng-go-btn" onClick={() => setGo(!go)} size={80} visible={!go} > {go ? 'Again' : 'Go'} </Button>
+            <Button className="rng-go-btn" onClick={() => genRandom()} size={80} visible={!go} > {go ? 'Again' : 'Go'} </Button>
 
             <div className={`random-number ${go ? 'random-number-show' : ''}`}>
                 <h1>{randomNumber}</h1>
